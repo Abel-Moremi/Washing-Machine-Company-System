8572da76dec49e849e53ed99f597f9cd20aad0b4
Abel Moremi <abelmoremiz@gmail.com>
2020-10-30 15:37:17 +0000

    add: getAssembles  to Assembles.java

--- src/backend/Assembles.java
+++ src/backend/Assembles.java
@@ -1,5 +1,18 @@
 package backend;
 
-public class Assembles {
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Abel Moremi
+ */
+
+public class Assembles extends DataConnection{
+
+    public ResultSet getAssembles(){
+        String stmt = "SELECT * FROM T_Assembles";
+        ResultSet rs = this.runStatement(stmt);
+        return rs;
+    }
     
 }
