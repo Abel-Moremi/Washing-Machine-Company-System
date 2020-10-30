8895c317440d4605f8c6c8a0ac8dd40841f393c3
Abel Moremi <abelmoremiz@gmail.com>
2020-10-30 14:20:54 +0000

    add: getEmployee to Employee.java

--- src/backend/Employee.java
+++ src/backend/Employee.java
@@ -8,9 +8,17 @@ import java.sql.ResultSet;
  */
 
 public class Employee extends DataConnection{
+
     public ResultSet getEmployees(){
         String stmt = "SELECT * FROM T_Employee";
         ResultSet rs = this.runStatement(stmt);
         return rs;
     }
+
+    public ResultSet getEmployee(String ENo){
+        String stmt = "SELECT * FROM T_Employee WHERE employee_ENo='"+ENo+"'";
+        ResultSet rs = this.runStatement(stmt);
+        return rs;
+    }
+
 }
