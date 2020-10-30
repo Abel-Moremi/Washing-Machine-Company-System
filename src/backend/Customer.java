d7c1a62bb7905e28fd3734f3e672e85b4a712f21
Abel Moremi <abelmoremiz@gmail.com>
2020-10-30 01:26:04 +0000

    add: d getCustomers to PaCustomer.java

--- src/backend/Customer.java
+++ src/backend/Customer.java
@@ -1,5 +1,17 @@
 package backend;
 
-public class Customer {
-    
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Abel Moremi
+ */
+
+public class Customer extends DataConnection{
+
+    public ResultSet getCustomers(){
+        String stmt = "SELECT * FROM T_Customer";
+        ResultSet rs = this.runStatement(stmt);
+        return rs;
+    }
 }
