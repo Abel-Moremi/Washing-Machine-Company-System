8d42e9f523b73347ebb08b7814401ba6d213320e
Abel Moremi <abelmoremiz@gmail.com>
2020-10-30 02:03:56 +0000

    add: getMachine to Machine.java

--- src/backend/Machine.java
+++ src/backend/Machine.java
@@ -1,5 +1,16 @@
 package backend;
 
-public class Machine {
-    
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Abel Moremi
+ */
+
+public class Machine extends DataConnection{
+    public ResultSet getMachine(){
+        String stmt = "SELECT WMC_NAME FROM T_Machine";
+        ResultSet rs = this.runStatement(stmt);
+        return rs;
+    }
 }
