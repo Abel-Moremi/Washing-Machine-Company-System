1af243f51e723aba3a1bbd21d7409b9231592a74
Abel Moremi <abelmoremiz@gmail.com>
2020-10-30 15:28:13 +0000

    add: updateProduct to Product.java

--- src/backend/Product.java
+++ src/backend/Product.java
@@ -16,14 +16,22 @@ public class Product extends DataConnection{
     }
 
     public ResultSet getProduct(String prNo){
-        String stmt = "SELECT * FROM T_PART WHERE product_MNo ='"+prNo+"'";
+        String stmt = "SELECT * FROM T_Product WHERE product_MNo ='"+prNo+"'";
         ResultSet rs = this.runStatement(stmt);
         return rs;
     }
 
     public void addProduct(String prNo, String name, String cost){
-        String addProductStmt = "INSERT INTO T_Part (product_MNo, product_PrName, product_cost)" 
+        String addProductStmt = "INSERT INTO T_Product (product_MNo, product_PrName, product_cost)" 
                 + "VALUES('"+prNo+"', '"+name+"', '"+cost+"')";
         this.runStatement(addProductStmt);
+    }
+
+    public void updateProduct(String prNo, String newName, String newCost){
+        String updateProductStmt = "UPDATE T_Product "
+                + "SET product_PrName = '"+newName+"', product_cost = '"+newCost+"'"
+                + "WHERE product_MNo ='"+prNo+"'";
+        
+        this.runStatement(updateProductStmt);
     }
 }
