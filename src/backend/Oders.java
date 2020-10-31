6b2a9cd9829f04cb662c77c491d7966c098a621b
Abel Moremi <abelmoremiz@gmail.com>
2020-10-31 09:08:13 +0000

    add: dgetOders  o AOders.java

--- src/backend/Oders.java
+++ src/backend/Oders.java
@@ -1,5 +1,19 @@
 package backend;
 
-public class Oders {
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Abel Moremi
+ */
+
+public class Oders extends DataConnection{
+
+    public ResultSet getOrders(){
+        String stmt = "SELECT * FROM T_Orders";
+        ResultSet rs = this.runStatement(stmt);
+        return rs;
+    }
+
     
 }
