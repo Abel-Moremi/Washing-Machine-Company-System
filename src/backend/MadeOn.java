f464b622fe0245f27193b2698b2978aea574c2fe
Abel Moremi <abelmoremiz@gmail.com>
2020-10-31 09:50:37 +0000

    add: madeOns to MadeON.java

--- src/backend/MadeOn.java
+++ src/backend/MadeOn.java
@@ -1,5 +1,17 @@
 package backend;
 
-public class MadeOn {
-    
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Abel Moremi
+ */
+
+public class MadeOn extends DataConnection{
+
+    public ResultSet getMadeOns(){
+        String stmt = "SELECT * FROM T_MadeOn";
+        ResultSet rs = this.runStatement(stmt);
+        return rs;
+    }
 }
