73884ac6b58aae5177e27469d9256f4ce6636ec2
Abel-Moremi <abelmoremiz@gmail.com>
2020-11-04 02:38:49 +0200

    fix: Home navigation issues

--- src/frontend/BaseFrame.java
+++ src/frontend/BaseFrame.java
@@ -8,13 +8,18 @@ package frontend;
 
 import javax.swing.JFrame;
 import java.awt.CardLayout;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.Box;
 
 /**
  *
  * @author Abel Moremi
  */
-public class BaseFrame extends JFrame {
+public class BaseFrame extends JFrame implements ActionListener{
     
     CardLayout cardLayout;
     JPanel base;
@@ -27,7 +32,7 @@ public class BaseFrame extends JFrame {
      */
     public BaseFrame() {
         
-        //initComponents();
+       
         
         cardLayout = new CardLayout();
         base = new JPanel(cardLayout);
@@ -38,9 +43,55 @@ public class BaseFrame extends JFrame {
         base.add(main, "main");
         base.add(customer, "customer");
         
+        
+        
+        
+        JButton customerButton = new JButton();
+        customerButton.setText("Customer");
+        customerButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                customerButtonActionPerformed(evt);
+            }
+        });
+        
+        JButton homeButton = new JButton();
+        homeButton.setText("Home");
+        homeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                homeButtonActionPerformed(evt);
+            }
+        });
+        
+        JButton employeeButton = new JButton();
+        employeeButton.setText("Employee");
+        employeeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                employeeButtonActionPerformed(evt);
+            }
+        });
+        
+        Box box = Box.createVerticalBox();
+        box.add(Box.createVerticalStrut(10));
+        box.add(customerButton);
+        box.add(Box.createVerticalStrut(10));
+        box.add(homeButton);
+        box.add(Box.createVerticalStrut(10));
+        box.add(employeeButton);
+        box.add(Box.createVerticalStrut(10));
+        
+        add(box, BorderLayout.WEST);
+        
         add(base);  
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationByPlatform(true);
+        setVisible(true);
+        
+        //initComponents();
      
     }
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -52,6 +103,8 @@ public class BaseFrame extends JFrame {
     private void initComponents() {
 
         customerButton = new javax.swing.JButton();
+        employeeButton = new javax.swing.JButton();
+        homeButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -62,21 +115,42 @@ public class BaseFrame extends JFrame {
             }
         });
 
+        employeeButton.setText("Employee");
+        employeeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                employeeButtonActionPerformed(evt);
+            }
+        });
+
+        homeButton.setText("Home");
+        homeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                homeButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(customerButton)
-                .addContainerGap(318, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(homeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(customerButton)
+                    .addComponent(employeeButton))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(12, 12, 12)
+                .addComponent(homeButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(employeeButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(customerButton)
-                .addContainerGap(266, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -86,6 +160,15 @@ public class BaseFrame extends JFrame {
         // TODO add your handling code here:
        cardLayout.show(base, "customer");
     }//GEN-LAST:event_customerButtonActionPerformed
+
+    private void employeeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_employeeButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_employeeButtonActionPerformed
+
+    private void homeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_homeButtonActionPerformed
+        // TODO add your handling code here:
+        cardLayout.show(base, "main");
+    }//GEN-LAST:event_homeButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -117,13 +200,20 @@ public class BaseFrame extends JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new BaseFrame().setVisible(true);
+                new BaseFrame();
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton customerButton;
+    private javax.swing.JButton employeeButton;
+    private javax.swing.JButton homeButton;
     // End of variables declaration//GEN-END:variables
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+         cardLayout.show(base, "customer");
+    }
 
 }
